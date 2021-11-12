89079bf3ff1a8ac6bf52320b9b9407499142bf52
xceska06 <xceska06@stud.fit.vutbr.cz>
2021-11-12 01:19:08 +0100

    Done

--- setcal.c
+++ setcal.c
@@ -2,124 +2,133 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-
-typedef struct {
+typedef struct
+{
     char **elements;
     int count;
 } Set;
 
-typedef struct {
+typedef struct
+{
     Set **sets;
     int setsCout;
     Set *universum;
-} Data;//rename in future
+} Data;
 
-void loadSet(FILE *file, Set *set){
+/**
+ * @brief nacte set do structu (max 100 setu maximalne 100 znaku dlouhych TODO:nekonecno znaku a setu)
+ *
+ * @param file ukazatel na soubor
+ * @param set
+ */
+void loadSet(FILE *file, Set *set)
+{
 
-    set->elements = (char**)malloc(100*sizeof(char*));
-    char c =0;
+    set->elements = (char **)malloc(100 * sizeof(char *));
+    char c = 0;
     int j = 0;
-    set->elements[0] = (char*)malloc(100*sizeof(char));
+    set->elements[0] = (char *)malloc(100 * sizeof(char));
 
-    for ( int i =0;(c =fgetc(file))!='\n'; i++)
+    for (int i = 0; (c = fgetc(file)) != '\n'; i++)
     {
-        if(c == EOF){
+        if (c == EOF)
+        {
             break;
         }
-        if(c==' '){
+        if (c == ' ')
+        {
             set->elements[j][i] = 0;
-             j++;
-             i=-1;
-             set->count = j+1;
-            set->elements[j] = (char*)malloc(100*sizeof(char));
+            j++;
+            i = -1;
+            set->count = j + 1;
+            set->elements[j] = (char *)malloc(100 * sizeof(char));
             continue;
         }
 
         set->elements[j][i] = c;
-
     }
 
     return;
-
 }
 
-// void AppendSet(Set set1, Set set2){
+/**
+ * @brief prints all in Data struct
+ *
+ * @param data
+ */
+void printData(Data data)
+{
 
-//     char* ptr;
-//     ptr = (char**) malloc((set1.count + set2.count)*sizeOf(char*));
+    printf("\n universum:\n\n");
+    for (int j = 0; j < data.universum->count; j++)
+    {
 
-//     for (int i = 0; set1.count<i; i++)
-//     {
-//         int sizeOfString;
-//         for(int j = 0; set1.elements[i] != 0; j++){
+        printf("%s",data.universum->elements[j]);
 
-//         }
-//        ptr[i]= (char**) malloc((set1.count + set2.count)*sizeOf(char*))set1.elements[i];
-//     }
+        printf(" ");
+    }
+    printf("\n sets: \n\n ");
+    for (int i = 0; i < data.setsCout; i++)
+    {
 
-//     return set;
+        for (int j = 0; j < data.sets[i]->count; j++)
+        {
 
-// }
+            printf("%s",data.sets[i]->elements[j]);
 
+            printf(" ");
+        }
+        printf("\n");
+    }
 
-Data Load(char file[]){
+    printf("\n");
+}
+
+/**
+ * @brief Nacte data ze souboru, a vrati je ve forme structu data
+ *
+ * @param file cesta k souboru
+ * @return Data
+ */
+Data Load(char file[])
+{
+    //inicializace
     Data data;
-    data.setsCout=0;
-     FILE *fp = fopen(file, "r");
+    data.setsCout = 0;
+    FILE *fp = fopen(file, "r");
     char c = 0;
-    data.sets = (Set**)malloc(100*sizeof(Set*));
-    data.universum = (Set*)malloc(sizeof(Set));
-    while((c = fgetc(fp))!=EOF){
-        switch(c){
+    data.sets = (Set **)malloc(100 * sizeof(Set *));
+    data.universum = (Set *)malloc(sizeof(Set));
 
-            case 'U':
-                  loadSet(fp, data.universum);
-                break;
-            case 'S':
-                  data.sets[data.setsCout] = (Set*)malloc(sizeof(Set));
-                 loadSet(fp,data.sets[data.setsCout]);
-                 data.setsCout++;
-                break;
-            case 'R': //TODO:
-                break;
-            case 'C': //TODO:
-                break;
+    //nacitani prvniho znaku na kazdym radku
+    while ((c = fgetc(fp)) != EOF)
+    {
+        switch (c)
+        {
+
+        case 'U':
+            loadSet(fp, data.universum);
+            break;
+        case 'S':
+            data.sets[data.setsCout] = (Set *)malloc(sizeof(Set));
+            loadSet(fp, data.sets[data.setsCout]);
+            data.setsCout++;
+            break;
+        case 'R': //TODO:
+            break;
+        case 'C': //TODO:
+            break;
         }
     }
-//printing for debug
-  printf("\n");
-    for(int i =0;i<data.setsCout; i++){
+    //printing for debug ----------------------- TODO: remove
 
-         for(int j =0;j<data.sets[i]->count; j++){
-
-
-                            printf(data.sets[i]->elements[j]);
-
-
-             printf(" ");
-           }
-            printf("\n");
-
-    }
- printf("\n universum\n ");
-        for(int j =0;j<data.universum->count; j++){
-
-
-                            printf(data.universum->elements[j]);
-
- printf(" ");
-
-           }
- printf("\n");
-
+    printData(data);
     return data;
-
 }
 
-
-
-int main(void){
+int main(void)
+{
 
     Load("test.txt");
-	return 0;
+    return 0;
 }
